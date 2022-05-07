9b35ecd31cc9bf3bd1c6385fd6695700d2650cda
FyckGG <84711438+FyckGG@users.noreply.github.com>
2022-05-07 22:36:16 +0700

    Add files via upload

---
+++ NotSymbpl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    static class NotSymbpl
+    {
+        static public string Not_symbol(string text)
+        {
+            {
+                string result = "";
+
+                string good_string = "";
+
+                string copy_text = text;
+
+                int lost_lenght = 0;
+
+                int pos = 0;
+
+                while (pos < text.Length)
+                {
+                    char c = text[pos];
+                    if (c != '&')
+                    {
+                        good_string += c;
+                      
+                    }
+                    
+
+                    else {
+
+                        if (good_string != "")
+
+                        {
+                            result += good_string + " позиция - " + (copy_text.IndexOf(good_string) + lost_lenght)+ ", ";
+                            lost_lenght += good_string.Length;
+                            copy_text = copy_text.Substring(good_string.Length);
+                            good_string = "";
+                        }
+                    }
+
+                    pos++;
+                }
+
+               if (good_string != "")
+                    result += good_string + " позиция - " + (copy_text.IndexOf(good_string) + lost_lenght) + ", ";
+               if (result == "")
+                    result += ("Совпадений не найдено");
+                return result;
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Regular.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Lab1
+{
+    static class Regular
+
+    {
+       static public string Check(string text, string reg, char s1, char s2)
+        {
+            string result = "";
+            
+            string copy_word;
+
+            int i;
+
+            //Regex first = new Regex("(0|1)*0(0|1)(0|1)");
+
+            Regex first = new Regex(reg);
+
+
+            MatchCollection matches = first.Matches(text);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    result += (match.Value) + " позиция - " + text.IndexOf(match.Value) + ", ";
+                    copy_word = match.Value;
+                    copy_word = copy_word.Replace(s1, '!');
+                    copy_word = copy_word.Replace(s2, '!');
+                    i = text.IndexOf(match.Value);
+                    text = text.Remove(i, match.Value.Length).Insert(i, copy_word);
+
+                }
+            }
+            else
+            {
+                result += ("Совпадений не найдено");
+            }
+                return result;
+            
+        }
+
+    }
+}
